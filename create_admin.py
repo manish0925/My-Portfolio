0c3cc3c81cdb9a593acd3ae1f7a10b552da69f4e
manish0925 <156315401+manish0925@users.noreply.github.com>
2026-04-27 23:12:41 +0530

    Create script to set up Django superuser

---
+++ create_admin.py
@@ -0,0 +1,13 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings') # apne project ka naam likhein
+django.setup()
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'password123')
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
